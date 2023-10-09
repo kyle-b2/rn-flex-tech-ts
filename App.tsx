f4d16f61c20a3dbe76582e81beb11bc6ee5632cb
kyle <kyle.broomfield@flexbase.app>
2023-10-09 15:07:37 -0400

    Move contents to parent folder

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type User = {
+  id: string;
+  name: string;
+  roles: string[];
+  email: string;
+};
+
+const API_BASE_URL = 'https://interview-api.flexbase.workers.dev';
+
 export default function App() {
   return (
     <View style={styles.container}>
